9263209b34061f57d20ecd4b42d01d1d8630b66e
Stefano <stefano.francolino@correo.ucu.edu.uy>
2024-11-26 23:36:54 -0300

    ...

--- App.js
+++ App.js
@@ -1,43 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Button, View, StyleSheet } from 'react-native';
-import User from './pages/User';
-import ProfileFriends from './pages/ProfileFriends';
+import { StyleSheet, View } from 'react-native';
+import TopHeader from './components/TopHeader';
+import BottomHeader from './components/BottomHeader';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import UserContext from './UserContext';
 
 const Stack = createStackNavigator();
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Button
-        title="Go to User"
-        onPress={() => navigation.navigate('User')}
-      />
-      <Button
-        title="Go to Profile Friends"
-        onPress={() => navigation.navigate('ProfileFriends')}
-      />
-    </View>
-  );
-}
-
 export default function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="User" component={User} />
-        <Stack.Screen name="ProfileFriends" component={ProfileFriends} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserContext.Provider value={{ user, setUser }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
   },
+  botttomContainer: {
+    justifyContent: 'flex-end',
+  }
 });
